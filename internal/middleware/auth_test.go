346ed300f5b7609b09a4dcf233e4c849b8813084
arvin-mf <arvinmulia55@gmail.com>
2026-06-13 21:45:39 +0700

    test: add middleware auth failed cases

--- internal/middleware/auth_test.go
+++ internal/middleware/auth_test.go
@@ -10,6 +10,8 @@ import (
 	pbauth "github.com/loanem-backend/protos/pb/proto/services/auth/v1"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func TestAuth(t *testing.T) {
@@ -18,10 +20,17 @@ func TestAuth(t *testing.T) {
 	validToken := "sample.valid.token"
 	fullToken := "Bearer " + validToken
 
+	sampleResponse := &pbauth.ValidateTokenResponse{
+		UserId:  21,
+		Name:    "Name Test",
+		IsValid: true,
+	}
+
 	tests := []struct {
 		name         string
 		mockBehavior func(m *server_mock.MockAuthServiceClient)
-		assertCase   func(t *testing.T, w *httptest.ResponseRecorder)
+		headerValue  string
+		assertCase   func(t *testing.T, w *httptest.ResponseRecorder, c *gin.Context)
 	}{
 		{
 			name: "Success_ValidToken",
@@ -30,14 +39,44 @@ func TestAuth(t *testing.T) {
 					ValidateToken(gomock.Any(), &pbauth.ValidateTokenRequest{
 						AccessToken: validToken,
 					}).
-					Return(&pbauth.ValidateTokenResponse{
-						UserId:  21,
-						Name:    "Name Test",
-						IsValid: true,
-					}, nil)
+					Return(sampleResponse, nil)
 			},
-			assertCase: func(t *testing.T, w *httptest.ResponseRecorder) {
+			headerValue: fullToken,
+			assertCase: func(t *testing.T, w *httptest.ResponseRecorder, c *gin.Context) {
 				assert.Equal(t, http.StatusOK, w.Code)
+				assert.NotContains(t, w.Body.String(), "unauthorized")
+				id, exists := c.Get("id")
+				assert.True(t, exists)
+				assert.Equal(t, sampleResponse.UserId, id)
+				name, exists := c.Get("name")
+				assert.True(t, exists)
+				assert.Equal(t, sampleResponse.Name, name)
+			},
+		},
+		{
+			name: "Failed_MissingHeader",
+			mockBehavior: func(m *server_mock.MockAuthServiceClient) {
+				m.EXPECT().ValidateToken(gomock.Any(), gomock.Any()).Times(0)
+			},
+			headerValue: "",
+			assertCase: func(t *testing.T, w *httptest.ResponseRecorder, c *gin.Context) {
+				assert.Equal(t, http.StatusUnauthorized, w.Code)
+				strBody := w.Body.String()
+				assert.Contains(t, strBody, "unauthorized")
+				assert.NotContains(t, strBody, "invalid or expired token")
+			},
+		},
+		{
+			name: "Failed_InvalidToken",
+			mockBehavior: func(m *server_mock.MockAuthServiceClient) {
+				m.EXPECT().
+					ValidateToken(gomock.Any(), gomock.Any()).
+					Return(nil, status.Error(codes.Internal, ""))
+			},
+			headerValue: fullToken,
+			assertCase: func(t *testing.T, w *httptest.ResponseRecorder, c *gin.Context) {
+				assert.Equal(t, http.StatusUnauthorized, w.Code)
+				assert.Contains(t, w.Body.String(), "invalid or expired token")
 			},
 		},
 	}
@@ -51,7 +90,10 @@ func TestAuth(t *testing.T) {
 			test.mockBehavior(mockAuthClient)
 
 			r := gin.New()
+			var ctx *gin.Context
 			r.GET("/sample-path", Auth(mockAuthClient), func(c *gin.Context) {
+				ctx = c
+
 				c.JSON(http.StatusOK, gin.H{
 					"ok": true,
 				})
@@ -59,11 +101,11 @@ func TestAuth(t *testing.T) {
 
 			w := httptest.NewRecorder()
 			req := httptest.NewRequest(http.MethodGet, "/sample-path", nil)
-			req.Header.Set("Authorization", fullToken)
+			req.Header.Set("Authorization", test.headerValue)
 
 			r.ServeHTTP(w, req)
 
-			test.assertCase(t, w)
+			test.assertCase(t, w, ctx)
 		})
 	}
 }
