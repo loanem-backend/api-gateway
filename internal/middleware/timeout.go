c4b5a2d89d00c596f3198d131e99d3d8dbe766ac
arvin-mf <arvinmulia55@gmail.com>
2026-06-13 22:40:26 +0700

    test: add middleware timeout test

--- internal/middleware/timeout.go
+++ internal/middleware/timeout.go
@@ -2,17 +2,32 @@ package middleware
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Timeout(d time.Duration) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		c, cancel := context.WithTimeout(ctx, d)
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c.Request.Context(), d)
 		defer cancel()
 
-		ctx.Request = ctx.Request.WithContext(c)
-		ctx.Next()
+		c.Request = c.Request.WithContext(ctx)
+
+		done := make(chan struct{})
+		go func() {
+			c.Next()
+			close(done)
+		}()
+
+		select {
+		case <-done:
+			return
+		case <-ctx.Done():
+			c.AbortWithStatusJSON(http.StatusRequestTimeout, gin.H{
+				"error": "request timeout",
+			})
+		}
 	}
 }
